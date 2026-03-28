3577bb58d1e17aaed51cd0a38af84287cc660a4c
apathydevil <76252555+apathydevil@users.noreply.github.com>
2026-03-28 15:51:56 +0100

    added a check for if there isn't any recent activity. then added a print statement accordingly.

--- display.py
+++ display.py
@@ -1,21 +1,24 @@
 
 def display(data):
     try:
-        for event in data:
-            timestamp = event['created_at'].replace('T', ' ').replace('Z', '')
-            if event['type'] == 'PushEvent':
-               print(f"Pushed to {event['repo']['name']} at {timestamp}")
-            elif event['type'] == 'CreateEvent':
-                print(f"Created {event['payload']['ref_type']} in {event['repo']['name']} at {timestamp}")
-            elif event['type'] == 'PullRequestEvent':
-                print(f"Pull request {event['payload']['action']} in {event['repo']['name']} at {timestamp}")
-            elif event['type'] == 'IssuesEvent':
-                print(f"Issue {event['payload']['action']} in {event['repo']['name']} at {timestamp}")
-            elif event['type'] == 'WatchEvent':
-                print(f"Starred {event['repo']['name']} at {timestamp}")
-            elif event['type'] == 'ForkEvent':
-                print(f"Forked {event['repo']['name']} at {timestamp}")
-            else:
-                print(f"{event['type']} in {event['repo']['name']} at {timestamp}")
+        if data is not None and len(data) > 0:
+            for event in data:
+                timestamp = event['created_at'].replace('T', ' ').replace('Z', '')
+                if event['type'] == 'PushEvent':
+                    print(f"Pushed to {event['repo']['name']} at {timestamp}")
+                elif event['type'] == 'CreateEvent':
+                    print(f"Created {event['payload']['ref_type']} in {event['repo']['name']} at {timestamp}")
+                elif event['type'] == 'PullRequestEvent':
+                    print(f"Pull request {event['payload']['action']} in {event['repo']['name']} at {timestamp}")
+                elif event['type'] == 'IssuesEvent':
+                    print(f"Issue {event['payload']['action']} in {event['repo']['name']} at {timestamp}")
+                elif event['type'] == 'WatchEvent':
+                    print(f"Starred {event['repo']['name']} at {timestamp}")
+                elif event['type'] == 'ForkEvent':
+                    print(f"Forked {event['repo']['name']} at {timestamp}")
+                else:
+                    print(f"{event['type']} in {event['repo']['name']} at {timestamp}")
+        else:
+            print("No recent activity found for this user.")
     except Exception as e:
         print(e)
