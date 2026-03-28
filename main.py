fd474350c8be942dd3be8241666def4a80ad31e9
apathydevil <76252555+apathydevil@users.noreply.github.com>
2026-03-28 15:46:03 +0100

    simple main.py cli implementation

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import display
 import fetcher
 
 def main():
-    username = "apathydevil"
+    print("Welcome to my github activity CLI!")
+    username = input("Please enter a github username to see their recent activity: ").strip().lower()
     data = fetcher.fetch(username)
     display.display(data)
 
